faf0c8e395976c80c88278fc85a8c36564ee57ff
Leandro LE <lean.uco@gmail.com>
2015-06-17 09:33:56 +0200

    Pagina Inmobiliaria haciendo modales

---
+++ models/Gallery.js
@@ -0,0 +1,22 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+/**
+ * Gallery Model
+ * =============
+ */
+
+var Gallery = new keystone.List('Gallery', {
+	autokey: { from: 'name', path: 'key', unique: true }
+});
+
+Gallery.add({
+	name: { type: String, required: true },
+	publishedDate: { type: Date, default: Date.now },
+	heroImage: { type: Types.CloudinaryImage },
+	images: { type: Types.CloudinaryImages },
+	precio: { type: Types.Text},
+	texto: {	type: Types.Textarea }
+});
+
+Gallery.register();
